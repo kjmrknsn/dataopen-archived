4709c4f6e9f7f3779e616441eb16903bf89ac8c0
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-20 19:14:20 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //! Collaborative Data Analysis Platform
 
 extern crate iron;
+extern crate router;
 
 /// Modules for Data Open Web
 pub mod web;
--- src/web/server.rs
+++ src/web/server.rs
@@ -1,11 +1,19 @@
 use iron::prelude::*;
 use iron::status;
+use router::Router;
+
+pub fn run() {
+    Iron::new(router()).http("localhost:3000").unwrap();
+}
+
+fn router() -> Router {
+    let mut router = Router::new();
+
+    router.get("/", hello_world, "index");
+
+    router
+}
 
 fn hello_world(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Hello, Data Open Web.")))
-}
-
-pub fn run() {
-    println!("On 3000");
-    Iron::new(hello_world).http("localhost:3000").unwrap();
 }
