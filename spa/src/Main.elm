e359082f93ca8c02af88a9a95b90d7ff0f7b0fa8
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-21 11:08:50 +0900

    Update

--- spa/src/Main.elm
+++ spa/src/Main.elm
@@ -1,182 +1,62 @@
-import Bootstrap.Button as Button
-import Bootstrap.Card as Card
-import Bootstrap.CDN as CDN
-import Bootstrap.Grid as Grid
-import Bootstrap.Grid.Col as Col
-import Bootstrap.Grid.Row as Row
-import Bootstrap.Modal as Modal
-import Bootstrap.Navbar as Navbar
-import Html exposing (Html, div, h1, h2, text)
-import Html.Attributes exposing (class, href)
-import Html.Attributes.Aria exposing (role)
-import Html.Events exposing (onClick)
-import Navigation exposing (Location)
-import UrlParser exposing ((</>))
+import Html exposing (Html, a, div, h1, li, text, ul)
+import Html.Attributes exposing (href)
+import Navigation
 
 
-main : Program Never Model Msg
 main =
     Navigation.program UrlChange
-        { view = view
+        { init = init
+        , view = view
         , update = update
-        , subscriptions = subscriptions
-        , init = init
+        , subscriptions = (\_ -> Sub.none)
         }
 
 
+-- MODEL
+
 type alias Model =
-    { page : Page
-    , navState : Navbar.State
-    , modalState : Modal.State
+    { history : List Navigation.Location
     }
 
 
-type Page
-    = Home
-    | GettingStarted
-
-
-init : Location -> ( Model, Cmd Msg )
+init : Navigation.Location -> ( Model, Cmd Msg)
 init location =
-    let
-        ( navState, navCmd ) =
-            Navbar.initialState NavMsg
-
-        ( model, urlCmd ) =
-            urlUpdate location { navState = navState, page = Home, modalState = Modal.hiddenState }
-    in
-        ( model, Cmd.batch [ urlCmd, navCmd ] )
+    ( Model [ location ]
+    , Cmd.none
+    )
 
 
-type Msg
-    = UrlChange Location
-    | NavMsg Navbar.State
-    | ModalMsg Modal.State
+-- UPDATE
+
+type Msg = UrlChange Navigation.Location
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Navbar.subscriptions model.navState NavMsg
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         UrlChange location ->
-            urlUpdate location model
-
-        NavMsg state ->
-            ( { model | navState = state }
-            , Cmd.none
-            )
-
-        ModalMsg state ->
-            ( { model | modalState = state }
+            ( { model | history = location :: model.history }
             , Cmd.none
             )
 
 
-urlUpdate : Navigation.Location -> Model -> ( Model, Cmd Msg )
-urlUpdate location model =
-    case decode location of
-        Nothing ->
-            ( { model | page = Home}, Cmd.none )
+-- VIEW
 
-        Just route ->
-            ( { model | page = route }, Cmd.none )
-
-
-decode : Location -> Maybe Page
-decode location =
-    UrlParser.parseHash routeParser location
-
-
-routeParser : UrlParser.Parser (Page -> a) a
-routeParser =
-    UrlParser.oneOf
-        [ UrlParser.map Home UrlParser.top
-        , UrlParser.map GettingStarted (UrlParser.s "getting-started")
-        ]
-
-
-view : Model -> Html Msg
+view : Model -> Html msg
 view model =
-    div [ class "h-100 bg-faded" ]
-        [ CDN.stylesheet
-        , menu model
-        , mainContent model
-        , modal model
+    div []
+        [ h1 [] [ text "Pages" ]
+        , ul [] (List.map viewLink [ "bears", "cats", "dogs", "elephants", "fish" ])
+        , h1 [] [ text "History" ]
+        , ul [] (List.map viewLocation model.history)
         ]
 
 
-menu : Model -> Html Msg
-menu model =
-    Navbar.config NavMsg
-        |> Navbar.withAnimation
-        |> Navbar.inverse
-        |> Navbar.brand [ href "#" ] [ text "Data Open" ]
-        |> Navbar.customItems
-            [ Navbar.customItem (Button.button [ Button.outlineSuccess, Button.attrs [ class "cursor-pointer", role "button" ] ] [ text "Sign In" ])
-            ]
-        |> Navbar.view model.navState
+viewLink : String -> Html msg
+viewLink name =
+    li [] [ a [ href ("#" ++ name) ] [ text name ] ]
 
 
-mainContent : Model -> Html Msg
-mainContent model =
-    Grid.containerFluid [ class "h-100" ] <|
-        case model.page of
-            Home ->
-                pageHome model
-
-            GettingStarted ->
-                pageGettingStarted model
-
-
-pageHome : Model -> List (Html Msg)
-pageHome model =
-    [ Grid.row [ Row.attrs [ class "mt-3" ] ]
-        [ Grid.col []
-            [ Card.config [ Card.attrs [ class "rounded-0" ] ]
-                |> Card.block []
-                    [ Card.titleH2 [] [ text "Data Open" ]
-                    , Card.text []
-                        [ text "Data Open is a web-based collaborative data analysis platform. Users can extract, analyze and visualize data by writing Spark application code and submitting it to a Hadoop cluster. Analysis results can be shared with others as a form of a notebook." ]
-                    ]
-                |> Card.view
-            ]
-        ]
-    ]
-
-
-pageGettingStarted : Model -> List (Html Msg)
-pageGettingStarted model =
-    [ h2 [] [ text "Getting started" ]
-    , Button.button
-        [ Button.success
-        , Button.large
-        , Button.block
-        , Button.attrs [ onClick <| ModalMsg Modal.visibleState ]
-        ]
-        [ text "Click me" ]
-    ]
-
-
-
-modal : Model -> Html Msg
-modal model =
-    Modal.config ModalMsg
-        |> Modal.small
-        |> Modal.h4 [] [ text "Getting started ?" ]
-        |> Modal.body []
-            [ Grid.containerFluid []
-                [ Grid.row []
-                    [ Grid.col
-                        [ Col.xs6 ]
-                        [ text "Col 1" ]
-                    , Grid.col
-                        [ Col.xs6 ]
-                        [ text "Col 2" ]
-                    ]
-                ]
-            ]
-        |> Modal.view model.modalState
+viewLocation : Navigation.Location -> Html msg
+viewLocation location =
+    li [] [ text (location.pathname ++ location.hash) ]
