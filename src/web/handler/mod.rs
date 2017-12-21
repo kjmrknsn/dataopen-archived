ab69dd27722ba041a4a315261b2ce4169e72d708
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-21 15:08:29 +0900

    Update

--- src/web/handler/mod.rs
+++ src/web/handler/mod.rs
@@ -3,8 +3,6 @@ use iron::mime;
 use iron::mime::{Attr, Mime, TopLevel, SubLevel};
 use iron::modifiers::Header;
 
-pub mod version;
-
 pub fn content_type() -> Header<ContentType> {
     Header(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, mime::Value::Utf8)])))
 }
--- src/web/router.rs
+++ src/web/router.rs
@@ -4,7 +4,5 @@ use super::handler;
 pub fn new() -> Router {
     let mut router = Router::new();
 
-    router.get("/api/version", handler::version::h, "version");
-
     router
 }
