ceae81eb73503d03d77efca429d73c12389af11d
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-21 13:22:02 +0900

    Update

--- spa/src/Main.elm
+++ spa/src/Main.elm
@@ -1,7 +1,7 @@
 import Html exposing (Attribute, Html, a, button, div, h1, li, nav, span, text, ul)
 import Html.Attributes exposing (attribute, class, href, id, type_)
 import Navigation
-
+import View.Nav as Nav
 
 main =
     Navigation.program UrlChange
@@ -45,24 +45,5 @@ update msg model =
 view : Model -> Html msg
 view model =
     div []
-        [ nav [ class "navbar navbar-expand-lg navbar-dark bg-dark" ]
-            [ a [ class "navbar-brand", href "#" ] [ text "Data Open" ]
-            , button
-                [ class "navbar-toggler"
-                , type_ "button"
-                , attribute "data-toggle" "collapse"
-                , attribute "data-target" "#navbarNav"
-                , attribute "aria-controls" "navbarNav"
-                , attribute "aria-expanded" "false"
-                , attribute "aria-label" "Toggle navigation"
-                ]
-                [ span [ class "navbar-toggler-icon" ] [] ]
-            , div [ class "collapse navbar-collapse", id "navbarNav" ]
-                [ ul [ class "navbar-nav mr-auto" ] []
-                , button
-                    [ class "btn btn-outline-success"
-                    , type_ "button" ]
-                    [ text "Sign In" ]
-                ]
-            ]
+        [ Nav.view
         ]
---
+++ spa/src/View/Nav.elm
@@ -0,0 +1,29 @@
+module View.Nav exposing (view)
+
+import Html exposing (Html, a, button, div, nav, span, text, ul)
+import Html.Attributes exposing (attribute, class, href, id, type_)
+
+-- VIEW
+
+view: Html msg
+view =
+    nav [ class "navbar navbar-expand-lg navbar-dark bg-dark" ]
+        [ a [ class "navbar-brand", href "#" ] [ text "Data Open" ]
+        , button
+            [ class "navbar-toggler"
+            , type_ "button"
+            , attribute "data-toggle" "collapse"
+            , attribute "data-target" "#navbarNav"
+            , attribute "aria-controls" "navbarNav"
+            , attribute "aria-expanded" "false"
+            , attribute "aria-label" "Toggle navigation"
+            ]
+            [ span [ class "navbar-toggler-icon" ] [] ]
+        , div [ class "collapse navbar-collapse", id "navbarNav" ]
+            [ ul [ class "navbar-nav mr-auto" ] []
+            , button
+                [ class "btn btn-outline-success"
+                , type_ "button" ]
+                [ text "Sign In" ]
+            ]
+        ]
